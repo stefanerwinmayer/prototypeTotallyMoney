b25589386613599bdd40be1fcae9a2934b4a0e4f
stefanem <stefan.e.mayer@gmail.com>
2018-04-12 00:31:38 +0100

    Latest.

--- src/Main.kt
+++ src/Main.kt
@@ -1,127 +1,154 @@
 import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
+
+sealed class Preference {
+    class DayOfMonth(val value: Int) : Preference()
+    class Weekdays(val days: List<DayOfWeek>) : Preference()
+    object Daily : Preference()
+    object Never : Preference()
+    object Unknown : Preference()
+}
+
+val dayOfWeekMap = mapOf(
+    "MON" to DayOfWeek.MONDAY,
+    "TUE" to DayOfWeek.TUESDAY,
+    "WED" to DayOfWeek.WEDNESDAY,
+    "THU" to DayOfWeek.THURSDAY,
+    "FRI" to DayOfWeek.FRIDAY,
+    "SAT" to DayOfWeek.SATURDAY,
+    "SUN" to DayOfWeek.SUNDAY
+)
+
+data class Customer(val tag: Char, val preferences: Preference)
+data class DailyReport(val date: LocalDate, val subscribers: List<Customer>)
 
 fun main(args: Array<String>) {
-    println("Input: ")
 
-    val inputs = readInputs()
+    greet()
 
-    val validInputs = inputs
+    val inputs = readFromConsole()
+
+    val preferences = inputs
         .filterNotNull()
-        .map { Input(it, getInputType(it)) }
-        .filter { it.type != InputTypes.InvalidInput }
+        .map { input -> getPreferences(input) }
 
-    val preferences: List<Preferences> = validInputs.map {
-        when (it.type) {
-            is InputTypes.DateInput -> Preferences.Date(LocalDate.now().withDayOfMonth(it.string.toInt()))
-            is InputTypes.WeekDayInput -> it.string.split(" ")
-                .map { DayOfWeek.valueOf(it) }
-                .let { Preferences.Weekdays(it) }
-            is InputTypes.NeverInput -> Preferences.Never
-            is InputTypes.DailyInput -> Preferences.EveryDay
-            else -> Preferences.Never
-        }
-    }
+    val alphabet = ('A'..'Z').iterator()
 
-    val numberInput = "29"
-    val datePattern = "^(2[08]|[12][0-9]|[1-9])\$".toRegex()
-    val hasDatePattern = datePattern.containsMatchIn(numberInput)
-    val number = numberInput.toInt()
-    val datePreference = Preferences.Date(LocalDate.now().withDayOfMonth(number))
+    val customers = preferences
+        .filterNot { it === Preference.Unknown }
+        .map { preference -> Customer(alphabet.nextChar(), preference) }
 
-    val weekDayInput = "WEDNESDAY THURSDAY"
-    val weekDayPattern = "MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY".toRegex()
-    val hasWeekDayPattern = weekDayPattern.containsMatchIn(weekDayInput)
-    val weekDayStrings = weekDayInput.split(" ")
-    val listDayOfWeek = weekDayStrings.map {
-        DayOfWeek.valueOf(it)
-    }
-    val weekDaysPreference = Preferences.Weekdays(listDayOfWeek)
+    val startDate = LocalDate.of(2018, 3, 31)
+    val fullReport = produceFullReport(startDate = startDate, customers = customers)
 
-    val neverInput = "Never"
-    val neverPattern = "Never".toRegex()
-    val hasNeverPattern = neverPattern.containsMatchIn(neverInput)
+    printReport(fullReport)
 
-    val dailyInput = "Daily"
-    val dailyPattern = "Daily".toRegex()
-    val hasDailyPattern = dailyPattern.containsMatchIn(dailyInput)
-
-    val allPreferences = listOf(datePreference, weekDaysPreference, Preferences.Never)
-
-    val start = LocalDate.now()
-    val secondDay = start.plusDays(1)
-    val end = start.plusDays(90)
-    val weekDay = start.dayOfWeek
 }
 
-fun readInputs(result: List<String?> = emptyList()): List<String?> =
+fun greet() {
+    println(
+        """
+        |Welcome to Customer Preference Centre
+        |
+        |Please enter a series of subscription getPreferences - one for each customer.
+        |You can enter:
+        |   1. [1-28] for date of month based subscriptions
+        |   2. [MONDAY-S
+        |   3. 'Daily' for every day subscriptions
+        |   4. 'Never' to no subscription
+        |
+        |Each customer's input will be terminated by pressing [Enter].
+        |Press [Enter] without any input to produce the report.
+        |Input not following either will be ignored.
+        |
+        |
+        |
+    """.trimMargin()
+    )
+}
+
+fun printReport(report: List<DailyReport>) {
+
+    for (daily in report) {
+        println(
+            "${daily.date.format(DateTimeFormatter.ofPattern("E dd-MMMM-yyyy"))}" +
+                    "   ${daily.subscribers
+                        .map { it.tag }
+                        .toString()
+                        .trim('[', ']')
+                    }"
+        )
+    }
+}
+
+fun produceFullReport(
+    startDate: LocalDate = LocalDate.now(),
+    endDate: LocalDate = startDate.plusDays(90),
+    customers: List<Customer>,
+    result: List<DailyReport> = emptyList()
+): List<DailyReport> =
+
+    if (startDate == endDate) result
+    else produceFullReport(
+        startDate = startDate.plusDays(1),
+        endDate = endDate,
+        customers = customers,
+        result = result.plus(
+            producDailyReport(
+                date = startDate.plusDays(1),
+                customers = customers
+            )
+        )
+    )
+
+
+fun producDailyReport(
+    date: LocalDate,
+    customers: List<Customer>
+): DailyReport =
+
+    DailyReport(
+        date = date,
+        subscribers = customers.mapNotNull { customer ->
+            when (customer.preferences) {
+                is Preference.DayOfMonth -> if (date.dayOfMonth == customer.preferences.value) customer else null
+                is Preference.Weekdays -> if (date.dayOfWeek in customer.preferences.days) customer else null
+                is Preference.Daily -> customer
+                is Preference.Never -> null
+                is Preference.Unknown -> null
+            }
+        }
+    )
+
+
+fun getPreferences(
+    input: String,
+    dayOfMonthPattern: Regex = "[0-9]".toRegex(),
+    weekDayPattern: Regex = "MON|TUE|WED|THU|FRI|SAT|SUN".toRegex(),
+    dailyPattern: Regex = "Daily".toRegex(),
+    neverPattern: Regex = "Never".toRegex()
+): Preference =
+
+    when {
+        dayOfMonthPattern in input -> Preference.DayOfMonth(input.toInt())
+        weekDayPattern in input -> input
+            .split(" ")
+            .mapNotNull { day -> dayOfWeekMap[day] }
+            .let { days -> Preference.Weekdays(days) }
+        dailyPattern in input -> Preference.Daily
+        neverPattern in input -> Preference.Never
+        else -> Preference.Unknown
+    }
+
+fun readFromConsole(result: List<String?> = emptyList()): List<String?> =
     readLine().let { input ->
         if (input == "") result
-        else readInputs(result.plus(input))
+        else readFromConsole(result.plus(input))
     }
 
 
-//fun classifyInputs(
-//    rawInputs: List<String?>,
-//    datePattern: Regex = "[0-9]".toRegex(),
-//    weekDayPattern: Regex = "MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY".toRegex(),
-//    neverPattern: Regex = "Never".toRegex(),
-//    dailyPattern: Regex = "Daily".toRegex()
-//): List<String> =
-//    rawInputs
-//        .filterNotNull()
-//        .map {
-//            when (i
-//        }
-
-fun getInputType(
-    input: String,
-    datePattern: Regex = "[0-9]".toRegex(),
-    weekDayPattern: Regex = "MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY".toRegex(),
-    neverPattern: Regex = "Never".toRegex(),
-    dailyPattern: Regex = "Daily".toRegex()
-): InputTypes = when {
-
-    input.contains(datePattern) -> InputTypes.DateInput
-    input.contains(weekDayPattern) -> InputTypes.WeekDayInput
-    input == "Never" -> InputTypes.NeverInput
-    input == "Daily" -> InputTypes.DailyInput
-    input == "" -> InputTypes.SubmitInput
-    else -> InputTypes.InvalidInput
-}
-
-fun validInputs(
-    rawInputs: List<String?>,
-    datePattern: Regex = "[0-9]".toRegex(),
-    weekDayPattern: Regex = "MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY".toRegex(),
-    neverPattern: Regex = "Never".toRegex(),
-    dailyPattern: Regex = "Daily".toRegex()
-): List<String> =
-    rawInputs
-        .filterNotNull()
-        .filter {
-            it.contains(datePattern) || it.contains(weekDayPattern) || it.contains(neverPattern) || it.contains(
-                dailyPattern
-            )
-        }
-
-
-val datePattern = "^(2[08]|[12][0-9]|[1-9])\$".toRegex()
-val weekDayPattern = "MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY".toRegex()
-val neverPattern = "Never".toRegex()
-val dailyPattern = "Daily".toRegex()
-
-
-//fun preference(input: List<String>): List<Preferences> =
-
-
-//fun verifyInput(input: String): Input =
-//    when (getInputType(input)) {
-//        InputTypes.DateInput, InputTypes.WeekDayInput, InputTypes.NeverInput, InputTypes.DailyInput -> Input.Valid
-//        InputTypes.SubmitInput -> Input.Submit
-//        InputTypes.InvalidInput -> Input.Invalid
-//    }
 
 
 
