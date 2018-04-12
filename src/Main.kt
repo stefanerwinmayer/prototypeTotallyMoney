1c77e9e853876d85df1b65d0be3217de8f04a998
stefanem <stefan.e.mayer@gmail.com>
2018-04-12 19:12:02 +0200

    Latest.

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,64 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 
+typealias Report = String
+
+data class DaySubscriptions(val date: LocalDate, val subscribers: List<Customer>)
+
+fun main(args: Array<String>) {
+
+    println(greetings)
+
+    val inputs: List<String> = readFromConsole()
+
+    val preferences: List<Preference> = inputs.map { each -> convertToPreferences(each) }
+
+    val customers: List<Customer> = makeCustomers(preferences)
+
+    val subscriptions: List<DaySubscriptions> = subscribersForRangeOfDates(
+        customers = customers,
+        startDate = LocalDate.of(2018, 3, 31)
+    )
+
+    val report: Report = compileReport(subscriptions)
+
+    println(report)
+
+}
+
+val greetings: String =
+    """
+        |Welcome to Customer Preference Centre
+        |
+        |Please enter a series of subscription preferences - one for each customer.
+        |
+        |Each customer's input will be terminated by pressing [Enter].
+        |Inputs not following conventions will be discarded.
+        |The conventions for entering preferences for a single customer are:
+        |
+        |   1. One of [01-28] to subscribe for a given day each month (Mind the preceding '0' for single digits)
+        |   2. One or more of [MON-SUN] to subscribe for one or more week days (separated by a space character)
+        |   3. 'Daily' to subscribe for every day
+        |   4. 'Never' to not subscribe at all
+        |
+        |
+        |Press [Enter] without any input to produce the report.
+        |
+    """.trimMargin()
+
+
+fun readFromConsole(
+    result: List<String?> = emptyList()
+): List<String> {
+
+    print("Enter customer preference > ")
+
+    return readLine().let { input: String? ->
+        if (input == "") result.filterNotNull()
+        else readFromConsole(result.plus(input))
+    }
+}
+
 sealed class Preference {
     class DayOfMonth(val value: Int) : Preference()
     class Weekdays(val days: List<DayOfWeek>) : Preference()
@@ -11,7 +69,26 @@ sealed class Preference {
     object Unknown : Preference()
 }
 
-val dayOfWeekMap = mapOf(
+fun convertToPreferences(
+    input: String,
+    dayOfMonthPattern: Regex = "0[0-9]|1[0-9]|2[0-8]".toRegex(),
+    weekDayPattern: Regex = "MON|TUE|WED|THU|FRI|SAT|SUN".toRegex(),
+    dailyPattern: Regex = "Daily".toRegex(),
+    neverPattern: Regex = "Never".toRegex()
+): Preference =
+
+    when {
+        dayOfMonthPattern in input -> Preference.DayOfMonth(input.toInt())
+        weekDayPattern in input -> input
+            .split(delimiters = " ")
+            .mapNotNull { day: String -> stringToDayOfWeek[day] }
+            .let { days: List<DayOfWeek> -> Preference.Weekdays(days) }
+        dailyPattern in input -> Preference.Daily
+        neverPattern in input -> Preference.Never
+        else -> Preference.Unknown
+    }
+
+val stringToDayOfWeek: Map<String, DayOfWeek> = mapOf(
     "MON" to DayOfWeek.MONDAY,
     "TUE" to DayOfWeek.TUESDAY,
     "WED" to DayOfWeek.WEDNESDAY,
@@ -21,100 +98,56 @@ val dayOfWeekMap = mapOf(
     "SUN" to DayOfWeek.SUNDAY
 )
 
-data class Customer(val tag: Char, val preferences: Preference)
-data class DailyReport(val date: LocalDate, val subscribers: List<Customer>)
+fun makeCustomers(
+    preferences: List<Preference>,
+    customerIds: CharIterator = ('A'..'Z').iterator()
+): List<Customer> =
 
-fun main(args: Array<String>) {
+    preferences
+        .filterNot { preference -> preference === Preference.Unknown }
+        .map { preference -> Customer(customerIds.nextChar(), preference) }
 
-    greet()
 
-    val inputs = readFromConsole()
+data class Customer(val id: Char, val preferences: Preference)
 
-    val preferences = inputs
-        .filterNotNull()
-        .map { input -> getPreferences(input) }
 
-    val alphabet = ('A'..'Z').iterator()
-
-    val customers = preferences
-        .filterNot { it === Preference.Unknown }
-        .map { preference -> Customer(alphabet.nextChar(), preference) }
-
-    val startDate = LocalDate.of(2018, 3, 31)
-    val fullReport = produceFullReport(startDate = startDate, customers = customers)
-
-    printReport(fullReport)
-
-}
-
-fun greet() {
-    println(
-        """
-        |Welcome to Customer Preference Centre
-        |
-        |Please enter a series of subscription getPreferences - one for each customer.
-        |You can enter:
-        |   1. [1-28] for date of month based subscriptions
-        |   2. [MONDAY-S
-        |   3. 'Daily' for every day subscriptions
-        |   4. 'Never' to no subscription
-        |
-        |Each customer's input will be terminated by pressing [Enter].
-        |Press [Enter] without any input to produce the report.
-        |Input not following either will be ignored.
-        |
-        |
-        |
-    """.trimMargin()
-    )
-}
-
-fun printReport(report: List<DailyReport>) {
-
-    for (daily in report) {
-        println(
-            "${daily.date.format(DateTimeFormatter.ofPattern("E dd-MMMM-yyyy"))}" +
-                    "   ${daily.subscribers
-                        .map { it.tag }
-                        .toString()
-                        .trim('[', ']')
-                    }"
-        )
-    }
-}
-
-fun produceFullReport(
+fun subscribersForRangeOfDates(
+    customers: List<Customer>,
     startDate: LocalDate = LocalDate.now(),
     endDate: LocalDate = startDate.plusDays(90),
-    customers: List<Customer>,
-    result: List<DailyReport> = emptyList()
-): List<DailyReport> =
+    result: List<DaySubscriptions> = emptyList()
+): List<DaySubscriptions> =
 
     if (startDate == endDate) result
-    else produceFullReport(
+    else subscribersForRangeOfDates(
+        customers = customers,
         startDate = startDate.plusDays(1),
         endDate = endDate,
-        customers = customers,
         result = result.plus(
-            producDailyReport(
-                date = startDate.plusDays(1),
-                customers = customers
-            )
+            subscribersForSingleDate(date = startDate.plusDays(1), customers = customers)
         )
     )
 
 
-fun producDailyReport(
+fun subscribersForSingleDate(
     date: LocalDate,
     customers: List<Customer>
-): DailyReport =
+): DaySubscriptions =
 
-    DailyReport(
+    DaySubscriptions(
         date = date,
         subscribers = customers.mapNotNull { customer ->
+
             when (customer.preferences) {
-                is Preference.DayOfMonth -> if (date.dayOfMonth == customer.preferences.value) customer else null
-                is Preference.Weekdays -> if (date.dayOfWeek in customer.preferences.days) customer else null
+
+                is Preference.DayOfMonth ->
+                    if (date.dayOfMonth == customer.preferences.value) customer
+                    else null
+
+                is Preference.Weekdays -> {
+                    if (date.dayOfWeek in customer.preferences.days) customer
+                    else null
+                }
                 is Preference.Daily -> customer
                 is Preference.Never -> null
                 is Preference.Unknown -> null
@@ -122,35 +155,22 @@ fun producDailyReport(
         }
     )
 
+fun compileReport(
+    subscriptions: List<DaySubscriptions>,
+    formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("E dd-MMMM-yyyy")
+): Report {
 
-fun getPreferences(
-    input: String,
-    dayOfMonthPattern: Regex = "[0-9]".toRegex(),
-    weekDayPattern: Regex = "MON|TUE|WED|THU|FRI|SAT|SUN".toRegex(),
-    dailyPattern: Regex = "Daily".toRegex(),
-    neverPattern: Regex = "Never".toRegex()
-): Preference =
+    val stringBuilder = StringBuilder()
 
-    when {
-        dayOfMonthPattern in input -> Preference.DayOfMonth(input.toInt())
-        weekDayPattern in input -> input
-            .split(" ")
-            .mapNotNull { day -> dayOfWeekMap[day] }
-            .let { days -> Preference.Weekdays(days) }
-        dailyPattern in input -> Preference.Daily
-        neverPattern in input -> Preference.Never
-        else -> Preference.Unknown
+    for (daySubscriptions in subscriptions) {
+        val date = daySubscriptions.date.format(formatter)
+        val subscriberIds = daySubscriptions.subscribers
+            .map { subscriber -> subscriber.id }
+            .toString()
+            .trim('[', ']')
+
+        stringBuilder.appendln("$date   $subscriberIds")
     }
 
-fun readFromConsole(result: List<String?> = emptyList()): List<String?> =
-    readLine().let { input ->
-        if (input == "") result
-        else readFromConsole(result.plus(input))
-    }
-
-
-
-
-
-
-
+    return stringBuilder.toString()
+}
